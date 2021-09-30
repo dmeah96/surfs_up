09e6d9c38bf94af6a16a5875733e333bf9e3d4fa
dmeah96 <dmeah96@gmail.com>
2021-09-29 22:56:22 -0500

    final

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-@app.route('/')
 def hello_world():
-    return 'Hello world';
-export FLASK_APP=app.py;
-set FLASK_APP=app.py;
+    return 'Hello world'
+    export FLASK_APP ==app.py
+set FLASK_APP=app.py
 
